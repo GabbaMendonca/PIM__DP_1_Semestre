f56ede6b65b15ce2e5c16d1042bf38265cd2b74c
GabrielMendonca <gabba.kumbuca@gmail.com>
2019-04-10 22:41:01 -0300

    inserido funcao de testes para pessoas e pesquisa de pessoas

--- main.c
+++ main.c
@@ -95,14 +95,72 @@ int numPalestra = 0;
     $STRUCT_FIM_CADASTRAR_PALESTRA$
     ===============================
 */
+/*
+    ==================================
+    $ STRUCT_INICIO_CADASTRAR_PESSOA $
+    ==================================
+*/
+
+/**
+ * @brief A struct contem os dados das pessoa a serem cadsatradas.
+ * Para possibilitar que mais de uma pessoa possa ser cadastrada
+ * foi inserido um vetor de scructs
+ * 
+ *      struct PESSOA pessoa[VETOR];
+ * 
+ * Assim o mesmo criar varios scructs podendo ser acessados atraves do
+ * numero do vetor.
+ * 
+ * Dessa forma cada pessoa ganha um numero numero de cadastro
+ * que posteriormente se torna a posição do vetor.
+ * 
+ */
+struct PESSOA 
+{   
+    long  cpf;
+    int  categoria;
+
+    char nome[SIZE];
+    int  idade;
+    char email[SIZE];    
+};
+
+struct PESSOA pessoa[VETOR];
+
+/**
+ * @brief Registra a quantidade total de cadstro
+ * 
+ */
+int contadorDeCadastros = 0;
+
+/**
+ * @brief Recebe o valor a ser acessdo na scruct
+ * 
+ */
+int numCadastro = 0;
+
+/*
+    ===============================
+    $ STRUCT_FIM_CADASTRAR_PESSOA $
+    ===============================
+*/
+/*
+    ============
+    FIM STRUCT'S
+    ============
+*/
+
+
+
+
 
 
 
 
 /*
-    ======================
-    PROTOTIPOS DAS FUNÇÕES
-    ======================
+    =============================
+    INICIO PROTOTIPOS DAS FUNÇÕES
+    =============================
 */
 /*
     ========================================
@@ -149,17 +207,70 @@ void alteraDadosPalestra();
  * @return int retorna o ano
  */
 int ano();
-    
+
 
 /*
-    =====================================
+    ========================================
     $ PROTOTIPOS_FIM_CADASTRAR_PALESTRA $
-    =====================================
+    ========================================
+*/
+/*
+    ========================================
+    $ PROTOTIPOS_INICIO_CADSATRAR_PESSOA $
+    ========================================
 */
 
 
+/**
+ * @brief Função principal que gerencia o cadastro de pessoas
+ * e a que deve ser chamada no menu.
+ * 
+ * O programa foi montado de maneira que as funções que inserem uma informação no sistema
+ * sejam independentes, de maneira que para alterar é necessario chamar a função que corresponde
+ * ao mesmo.
+ * 
+ * Esta função gerencia as principais tomadas de decisão em cadastro de pessoas.
+ * 
+ */
+void cadastrarPessoa();
 
 
+/**
+ * @brief Função contem a sequencia de cadastro.
+ * 
+ * @param categoria o paramento vem da função cadastar possoa e indica o tipo de pessoa a ser cadastrado.
+ */
+void Pessoa(int categoria);
+
+void inserirCpf();
+void inserirNome();
+void inserirIdade();
+void inserirEmail();
+
+/**
+ * @brief Apenas exibe o cadastro de acordo com o valor na variavel numCadastro
+ * 
+ */
+void exibirPessoa();
+
+/**
+ * @brief Contem a rotina de alterar dados caso ja cadastrados no sistema caso seja preciso.
+ * 
+ * @obs A posicao do vetor na variavel global numCadastro ja deve ter sido ajustado para a posição desejada.
+ * 
+ */
+void alteraDadosPessoa();
+
+/*
+    ========================================
+    $ PROTOTIPOS_INICIO_CADSATRAR_PESSOA $
+    ========================================
+*/
+/*
+    ==========================
+    FIM PROTOTIPOS DAS FUNÇÕES
+    ==========================
+*/
 
 
 
@@ -467,10 +578,261 @@ void alteraDadosPalestra()
     $ FUNCAO_FIM_CADASTRAR_PALESTRA $
     =================================
 */
+/*
+    ====================================
+    $ FUNCAO_INICIO_CADASTRAR_PESSOA $
+    ====================================
+*/
+
+void cadastrarPessoa()
+{
+
+    LIMPA_TERM
+    printf("Numero do Cadastro : %d\n\n", (contadorDeCadastros + 1));
+    numCadastro = contadorDeCadastros;
+
+    printf("Você quer cadastrar um ?\n\n");
+
+    printf("(1) >>> Usuario Comum\n");
+    printf("(2) >>> Professor\n");
+    printf("(3) >>> Convidado\n");
+    printf("(4) >>> Deficiente\n\n");
+
+    printf("(0) <<< Voltar\n\n");
+
+    int opc;
+    printf("Opção: ");
+    scanf("%d%*c", &opc);
+    fflush(stdin);
+
+        switch (opc)
+        {
+            case 0:
+                return;
+
+            case 1:
+                Pessoa(1);
+            break;
+
+            case 2:
+                Pessoa(2);
+            break;
+
+            case 3:
+                Pessoa(3);
+            break;
+
+            case 4:
+                Pessoa(4);
+            break;
+
+            default:
+                break;
+        }
+
+
+/*
+    ======
+*/
+
+
+    do
+    {
+        LIMPA_TERM
+        exibirPessoa();
+        
+        printf("\n\nDados estão corretos ? ( S / N ) : ");
+
+        char opc2;
+        opc2 = getchar();
+        fflush(stdin);
+
+        switch(opc2)
+        {
+
+            case 's':
+            case 'S':
+
+                printf("Dados gravados com sucesso !\n");
+                // GRAVAR DADOS
+                contadorDeCadastros++;
+                printf("Pressione ENTER para sair ... !");
+                getchar();
+            return;
+
+            case 'n':
+            case 'N':
+                alteraDadosPessoa();
+            break;
+
+            default:
+                printf("Opcão invalida!");
+            break;
+        }
+    }
+    while(1);
+
+
+}
 
 
 
 
+/*
+    ======
+*/
+
+
+
+
+void Pessoa(int categoria)
+{
+    pessoa[numCadastro].categoria = categoria;
+
+    LIMPA_TERM
+    exibirPessoa();
+    printf("\n\n");
+    inserirCpf();
+
+    LIMPA_TERM
+    exibirPessoa();
+    printf("\n\n");
+    inserirNome();
+
+    LIMPA_TERM
+    exibirPessoa();
+    printf("\n\n");
+    inserirIdade();
+
+    LIMPA_TERM
+    exibirPessoa();
+    printf("\n\n");
+    inserirEmail();
+
+}
+
+void inserirCpf()
+{
+    printf("Digite seu CPF: ");
+    scanf("%ld%*c", &pessoa[numCadastro].cpf);
+    fflush(stdin);
+}
+
+void inserirNome()
+{   
+    printf("Digite seu nome: ");
+    fgets(pessoa[numCadastro].nome, SIZE, stdin);
+    fflush(stdin);
+}
+
+void inserirIdade()
+{
+    printf("Digite sua idade: ");
+    scanf("%d%*c", &pessoa[numCadastro].idade);
+    fflush(stdin);
+}
+
+void inserirEmail()
+{
+    printf("Digite seu email: ");
+    fgets(pessoa[numCadastro].email, SIZE, stdin);
+    fflush(stdin);
+}
+
+
+
+
+/*
+    ======
+*/
+
+
+
+
+
+void exibirPessoa()
+{
+
+    char categoria[16];
+    
+    if(pessoa[numCadastro].categoria == 1)
+    {
+        strcpy(categoria, "Usuario Comum");
+    }
+    if(pessoa[numCadastro].categoria == 2)
+    {
+        strcpy(categoria, "Professor");
+    }
+    if(pessoa[numCadastro].categoria == 3)
+    {
+        strcpy(categoria, "Convidado");
+    }
+    if(pessoa[numCadastro].categoria == 4)
+    {
+        strcpy(categoria, "Deficiente");
+    }
+    
+
+    LIMPA_TERM
+    printf("CPF ............... : %ld\n", pessoa[numCadastro].cpf);
+    printf("Nome .............. : %s\n",   pessoa[numCadastro].nome);
+    printf("Idade ............. : %d\n", pessoa[numCadastro].idade);
+    printf("Email ............. : %s\n",   pessoa[numCadastro].email);
+    printf("Categoria ......... : %s\n",   categoria);
+
+}
+
+
+
+void alteraDadosPessoa()
+{
+    LIMPA_TERM
+    printf("( 1 ) >>> CPF ............... : %ld\n", pessoa[numCadastro].cpf);
+    printf("( 2 ) >>> Nome .............. : %s\n",   pessoa[numCadastro].nome);
+    printf("( 3 ) >>> Idade ............. : %d\n", pessoa[numCadastro].idade);
+    printf("( 4 ) >>> Email ............. : %s\n",   pessoa[numCadastro].email);
+    printf("( 5 ) >>> Categoria ......... : %d\n",   pessoa[numCadastro].categoria);
+
+    printf("\n( 0 ) <<< Voltar\n");
+
+    printf("\nDigite a opcção que deseja alterar ?");
+
+    int opc;
+    scanf("%d%*c", &opc);
+
+    
+    switch (opc)
+    {
+        case 0:
+            return;
+
+        case 1:
+            inserirCpf();
+        break;
+
+        case 2:
+            inserirNome();
+        break;
+
+        case 3:
+            inserirIdade();
+        break;
+
+        case 4:
+            inserirEmail();
+        break;
+
+        default:
+            printf("Opc invalida");
+        break;
+    }
+}
+
+/*
+    ====================================
+    $ FUNCAO_FIM_CADASTRAR_PESSOA $
+    ====================================
+*/
 /*
     ======================================
     $ FUNCAO_INICIO_PESQUISA_DE_PALESTRA $
@@ -557,7 +919,89 @@ void pesquisarPalestra()
     $ FUNCAO_FIM_PESQUISA_DE_PALESTRA $
     ======================================
 */
+/*
+    ======================================
+    $ FUNCAO_INICIO_PESQUISA_DE_PESSOA $
+    ======================================
+*/
 
+void mostarTodasAsPessoas()
+{
+
+    printf("| Num  |  Pessoas\n\n");
+
+    for(int i = 0; i <= contadorDeCadastros; i++)
+    {
+        if(i < 9)
+        {
+            printf("| 0%d   >  %s\n", (i + 1), pessoa[i].nome);
+        }
+        else
+        {
+            printf("| %d   >  %s\n", (i + 1), pessoa[i].nome);
+        }
+        
+    }
+
+}
+
+void pesquisarPessoa()
+{
+
+    if(pessoa[0].cpf == 0)
+    {
+        printf("Nenhum usuario esta cadastrado");
+    }
+    else
+    {
+
+        mostarTodasAsPessoas();
+
+        printf("\nDigite o ID da pessoa para ver os detalhes : ");
+
+        int opc;
+        scanf("%d%*c", &opc);
+        fflush(stdin);
+
+        numCadastro = opc - 1;
+
+        exibirPessoa();
+
+        printf("\n\nDeseja confirmar  ? ( S / N ) : ");
+
+        char opc2;
+        opc2 = getchar();
+        fflush(stdin);
+
+        switch(opc2)
+        {
+            case 's':
+            case 'S':
+
+                printf("Função ainda não produzida!");
+                getchar();
+                getchar();
+            return;
+
+            case 'n':
+            case 'N':
+                printf("Função ainda não produzida!");
+                getchar();
+                getchar();
+            break;
+
+            default:
+                printf("Opcão invalida!");
+            break;
+        }
+    }
+}
+
+/*
+    ======================================
+    $ FUNCAO_FIM_PESQUISA_DE_PESSOA $
+    ======================================
+*/
 
 
 /*
@@ -568,33 +1012,58 @@ void pesquisarPalestra()
 
 void teste_de_palestra()
 {
-    
-        strcpy(palestra[0].nomePalestra, "Ana");
-        strcpy(palestra[1].nomePalestra, "Beatriz");
-        strcpy(palestra[2].nomePalestra, "Juliaz");
-        strcpy(palestra[3].nomePalestra, "Bianca");
-        strcpy(palestra[4].nomePalestra, "Rosa");
-        strcpy(palestra[5].nomePalestra, "Sabrina");
-        strcpy(palestra[6].nomePalestra, "Leona");
-        strcpy(palestra[7].nomePalestra, "Vayne");
-        strcpy(palestra[8].nomePalestra, "Diana");
-        strcpy(palestra[9].nomePalestra, "She-ra");
 
-        for(int j = 0; j < 10; j++)
-        {
-            strcpy(palestra[j].campus, "UNIP");
-            palestra[j].dia = 11 + j;
-            palestra[j].mes = 1 + j;
-            palestra[j].ano = 2021 + j;
+    strcpy(palestra[0].nomePalestra, "Palestra 1");
+    strcpy(palestra[1].nomePalestra, "Palestra 2");
+    strcpy(palestra[2].nomePalestra, "Palestra 3");
+    strcpy(palestra[3].nomePalestra, "Palestra 4");
+    strcpy(palestra[4].nomePalestra, "Palestra 5");
+    strcpy(palestra[5].nomePalestra, "Palestra 6");
+    strcpy(palestra[6].nomePalestra, "Palestra 7");
+    strcpy(palestra[7].nomePalestra, "Palestra 8");
+    strcpy(palestra[8].nomePalestra, "Palestra 9");
+    strcpy(palestra[9].nomePalestra, "Palestra 10");
 
-            palestra[j].hora = 11 + j;
-            palestra[j].min = 21 + j;
-        }
+    for(int j = 0; j < 10; j++)
+    {
+        strcpy(palestra[j].campus, "UNIP");
+        palestra[j].dia = 11 + j;
+        palestra[j].mes = 1 + j;
+        palestra[j].ano = 2021 + j;
 
-        contadorDePalestra = 9;
+        palestra[j].hora = 11 + j;
+        palestra[j].min = 21 + j;
+    }
+
+    contadorDePalestra = 9;
 
 }
 
+
+void teste_de_pessoa()
+{
+    strcpy(pessoa[0].nome, "Ana");
+    strcpy(pessoa[1].nome, "Beatriz");
+    strcpy(pessoa[2].nome, "Juliaz");
+    strcpy(pessoa[3].nome, "Bianca");
+    strcpy(pessoa[4].nome, "Rosa");
+    strcpy(pessoa[5].nome, "Sabrina");
+    strcpy(pessoa[6].nome, "Leona");
+    strcpy(pessoa[7].nome, "Vayne");
+    strcpy(pessoa[8].nome, "Diana");
+    strcpy(pessoa[9].nome, "She-ra");
+
+    for(int j = 0; j < 10; j++)
+    {
+        pessoa[j].cpf = 11111111111;
+        pessoa[j].idade = 21 + j;
+        pessoa[j].categoria = 1;
+
+        strcpy(pessoa[j].email, "unip.unip@unip.com");
+    }
+
+    contadorDeCadastros = 9;
+}
 /*
     ============
     $$ TESTES $$
@@ -618,15 +1087,18 @@ void main()
         LIMPA_TERM
         printf("\n\n");
         printf("1 - Testar Cadastrar Palestra\n");
-        //printf("2 - Testar Cadastrar Pessoa\n");
+        printf("2 - Testar Cadastrar Pessoa\n");
 
         printf("\n\n");
         printf("Funçoes de Teste");
         printf("\n");
 
-        printf("8 - Popular Banco de Dados de Palestras\n");
-        printf("9 - Testar Banco de Dados Cadastro\n");
-
+        printf("7  - Popular Banco de Dados de Pessoas\n");
+        printf("8  - Popular Banco de Dados de Palestras\n");
+        printf("\n");
+        printf("9  - Testar Banco de Dados Passoas\n");
+        printf("10 - Testar Banco de Dados Palestra\n");
+        
         
         // printf("3 - Testar Menu Cadstro\n\n");
         // printf("3 - Testar Menu Cadstro\n\n");
@@ -642,20 +1114,26 @@ void main()
             break;
                 
             case 2:
-                //cadastrarPessoa();
+                cadastrarPessoa();
             break;
                 
             case 3:
                 //menuPalestra();
             break;
 
-
+            case 7:
+                teste_de_pessoa();
+            break;
 
             case 8:
                 teste_de_palestra();
             break;
 
             case 9:
+                pesquisarPessoa();
+            break;
+
+            case 10:
                 pesquisarPalestra();
             break;
 
